9839a3b4a7200a2db8c300c223be9de5bf37e65a
sargoo <goon_aguea@hotmail.com>
2024-08-16 21:09:57 -0300

    se crean selectores para los input

---
+++ js/app.js
@@ -0,0 +1,6 @@
+//selectores
+const pacienteInput = document.querySelector('#paciente');
+const propietarioInput = document.querySelector('#propietario');
+const emailInput = document.querySelector('#email');
+const fechaInput = document.querySelector('#fecha');
+const sintomasInput = document.querySelector('#sintomas');
