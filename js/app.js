2da4315ed31755363ef1fd74798114c89c72353b
sargoo <goon_aguea@hotmail.com>
2024-08-19 21:18:24 -0300

    se crea version mas dinamica para escribir el objeto de cita

--- js/app.js
+++ js/app.js
@@ -1,21 +1,43 @@
 //selectores
-const pacienteInput = document.querySelector('#paciente');
+const pacienteInput = document.querySelector('#mascota');
 const propietarioInput = document.querySelector('#propietario');
-const emailInput = document.querySelector('#email');
+const telefonoInput = document.querySelector('#telefono');
 const fechaInput = document.querySelector('#fecha');
+const horaInput = document.querySelector('#hora');
 const sintomasInput = document.querySelector('#sintomas');
 
 const citaObj = {
-    paciente: '',
+    mascota: '',
     propietario: '',
-    email: '',
+    telefono: '',
     fecha: '',
+    hora: '',
     sintomas: ''
 }
 
 //eventos
 
-pacienteInput.addEventListener('change', ()=> {
-    console.log('hola...');
-    
-})
+pacienteInput.addEventListener('change', (e)=> {
+    citaObj[e.target.name] = e.target.value;
+    console.log(citaObj);   
+});
+propietarioInput.addEventListener('change', (e)=> {
+    citaObj[e.target.name] = e.target.value;
+    console.log(citaObj);   
+});
+telefonoInput.addEventListener('change', (e)=> {
+    citaObj[e.target.name] = e.target.value;
+    console.log(citaObj);   
+});
+fechaInput.addEventListener('change', (e)=> {
+    citaObj[e.target.name] = e.target.value;
+    console.log(citaObj);   
+});
+horaInput.addEventListener('change', (e)=> {
+    citaObj[e.target.name] = e.target.value;
+    console.log(citaObj);   
+});
+sintomasInput.addEventListener('change', (e)=> {
+    citaObj[e.target.name] = e.target.value;
+    console.log(citaObj);   
+});
