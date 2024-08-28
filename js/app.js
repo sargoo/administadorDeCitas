48f39bf1377f3b769eb51d70305ead5905708dd1
sargoo <goon_aguea@hotmail.com>
2024-08-27 22:27:45 -0300

    se soluciona problema de alertas duiplicadas

--- js/app.js
+++ js/app.js
@@ -31,11 +31,10 @@ function subimitCita(e){
     e.preventDefault();
 
     if (Object.values(citaObj).some(valor => valor.trim() === '')) {
-        const notificacion = new Notificacion({
+        new Notificacion({
             texto: 'Todos los campos son obligatorios',
             tipo: 'error'
         })
-        notificacion.mostrar();
         return;
     }
 }
@@ -45,6 +44,8 @@ class Notificacion {
     constructor({texto, tipo}){
         this.texto = texto,
         this.tipo = tipo
+
+        this.mostrar();
     }
 
     mostrar(){
@@ -53,17 +54,27 @@ class Notificacion {
         const alerta = document.createElement('DIV');
         alerta.classList.add('text-center','w-full', 'p-3','text-white', 'my-5', 'alert', 'uppercase', 'font-bold', 'text-sm');
 
+        //eliminar alertas duplicadas
+
+        const alertaPrevia = document.querySelector('.alert');
+        alertaPrevia?.remove();
+        
+
         //si es de tipo error agrega una clase
 
-        this.tipo = 'error'? alerta.classList.add('bg-red-500') : alerta.classList.add('bg-green-500');
+        this.tipo === 'error'? alerta.classList.add('bg-red-500') : alerta.classList.add('bg-green-500');
 
         alerta.textContent = this.texto;
 
         //insertar en el dom
 
-        console.log('Formulario:', formulario);  // Verifica que formulario esté definido y accesible
-
         document.body.appendChild(alerta);
         formulario.parentElement.insertBefore(alerta, formulario);
+
+        //quitar despues de 5 segundos
+
+        setTimeout(()=>{
+            alerta.remove();
+        },3000);
     }
 }
